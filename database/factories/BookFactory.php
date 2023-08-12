95a364cb256e2eaf98b4348685849fc650a291ae
YuyaYamazi <kaimomo12345@gmail.com>
2023-08-12 17:01:19 +0900

    ダミーデータ,book.index作成

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $fileName = $this->faker->colorName() . '.jpg';
+        return [
+            'name' => $this->faker->name(),
+            'status' => $this->faker->numberBetween(1,4),
+            'author' => $this->faker->name(),
+            'image' => $fileName,
+            'publication' => $this->faker->name(),
+            'read_at' => $this->faker->date(),
+            'note' => $this->faker->realText(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+        ]);
+
+        Book::factory(100)->create();
     }
 }
